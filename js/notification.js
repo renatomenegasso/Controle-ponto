33906fd26c917807b5d42c306443247033ed55c2
samuel teixeira <samuelteixeira@great.ufc.br>
2014-04-01 13:50:37 -0300

    Ajustes para pegar do storage já guardado anteriormente

--- js/notification.js
+++ js/notification.js
@@ -3,52 +3,19 @@
   permission in the manifest file (or calling
   "webkitNotifications.requestPermission" beforehand).
 */
-
-function loadEndDay(){
-
-  var baseDate = new Date();
-  var time = new Date(baseDate.getFullYear(), baseDate.getMonth(), baseDate.getDate()).getTime();
-  var endDay;
-
-  var endDayValue = $("#"+time).find(".end-day").find("input").val();
-  var placeholderEndDay = $("#"+time).find(".end-day").find("input").attr("placeholder");
-
-  if(endDayValue != "" && endDay!= undefined)
-      var endDay = endDayValue;
-  else
-      var endDay = placeholderEndDay;
-
-  // store in localStorage to run in background
-  if(endDay != "" && endDay!= undefined){
-      console.log("guardar no localStorage");
-      localStorage.setItem("endDay", endDay);
-  }
-  else if(endDay == "" || endDay == undefined){
-      var localEndDay = localStorage.getItem("endDay");
-      if(localEndDay != "" || localEndDay!= undefined){
-          endDay = localEndDay;
-      }
-  }
-
-  return endDay;
-
-}
-
-
 function show() {
 
   var baseDate = new Date();
-  var notificationHour = baseDate.getHours()+":"+baseDate.getMinutes();
+  var time = new Date(baseDate.getFullYear(), baseDate.getMonth(), baseDate.getDate()).getTime();
+  var storageDay = localStorage.getItem(time);
+      storageDay = jQuery.parseJSON(storageDay);
 
-  // for test fix one hour; Example: notificationHour="17:13";
 
-  var endDay = loadEndDay();
-
-  if (endDay != undefined && notificationHour == endDay){
+  if (baseDate.getTime() == storageDay["saida"]){
     var notification = window.webkitNotifications.createNotification(
-      'res/icon_128.png',            // The image.
-      endDay,                 // The title.
-      'Time to go home.'      // The body.
+      'res/icon_128.png',                                     // The image.
+      baseDate.getHours()+":"+baseDate.getMinutes(),          // The title.
+      'Time to go home.'                                      // The body.
     );
     notification.show();
 
